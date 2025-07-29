948be7771f2177c25a6bf741cfc19a7a2f72e999
Florent 'Skia' Jacquet <florent.jacquet@canonical.com>
2025-07-29 12:19:02 +0200

    tools/remove_an_oops.py: fissix + black + improvements
    Improvements include:
    * Make use of f-strings
    * Support also deleting the OOPS from the UserOOPS table

---
+++ src/tools/remove_an_oops.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+# Remove a specific crash report from the OOPS and UserOOPS tables in the Error Tracker
+
+import sys
+
+from cassandra import ConsistencyLevel, OperationTimedOut
+from cassandra.cluster import Cluster, NoHostAvailable
+from cassandra.auth import PlainTextAuthProvider
+
+from time import sleep
+
+from daisy import config
+
+auth_provider = PlainTextAuthProvider(
+    username=config.cassandra_username, password=config.cassandra_password
+)
+cluster = Cluster(config.cassandra_hosts, auth_provider=auth_provider)
+session = cluster.connect(config.cassandra_keyspace)
+session.default_consistency_level = ConsistencyLevel.LOCAL_ONE
+
+URL = "https://errors.ubuntu.com/oops/"
+
+# Main
+if __name__ == "__main__":
+    if "--dry-run" in sys.argv:
+        dry_run = True
+        sys.argv.remove("--dry-run")
+    else:
+        dry_run = False
+
+    oopsid = sys.argv[1]
+    data = {}
+
+    oops_lookup_stmt = session.prepare('SELECT * FROM "OOPS" WHERE key=?')
+    oops_delete_stmt = session.prepare('DELETE FROM "OOPS" WHERE key=?')
+    useroops_delete_stmt = session.prepare(
+        'DELETE FROM "UserOOPS" WHERE key=? AND column1=?'
+    )
+
+    max_retries = 5
+    for i in range(max_retries):
+        period = 30 + (30 * i)
+        try:
+            oops_data = session.execute(oops_lookup_stmt, [oopsid.encode()])
+        except (OperationTimedOut, NoHostAvailable):
+            print(f"Sleeping {period}s as we timed out when querying.")
+            sleep(period)
+            continue
+        else:
+            break
+    else:
+        print(f"Cassandra operation timed out {max_retries} times.")
+        sys.exit(1)
+    # all the column "names" are column1 so make a dictionary of keys: values
+    for od in oops_data:
+        data[od.column1] = od.value
+    systemid = data["SystemIdentifier"]
+
+    if not dry_run:
+        for i in range(max_retries):
+            period = 30 + (30 * i)
+            try:
+                session.execute(oops_delete_stmt, [oopsid.encode()])
+                session.execute(useroops_delete_stmt, [systemid.encode(), oopsid])
+                print(f"{URL}{oopsid} was from {systemid} and had its data removed")
+            except (OperationTimedOut, NoHostAvailable):
+                print(f"Sleeping {period}s as we timed out when deleting.")
+                sleep(period)
+                continue
+            else:
+                break
+    else:
+        print(f"{URL}{oopsid} was from {systemid} and would have had its data removed")
